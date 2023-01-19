d2bbab4e92a57b5cdc1de6808fa5ea2aa402866b
Peyton C <peyton.castill0@icloud.com>
2023-01-19 14:27:39 -0500

    bundling the idex.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,37 +1,10 @@
 // this is where i will need 
 // to export my models 
 // the website will have an 
+const User = require('./User');
+const Article = require('./Article');
+const Comment = require('./Comment');
+const Admin = require('./Admin');
 
-// admin 
-// who can post and edit articles
-// and delete any comment
-// username
-// password
-// comments [commentSchema]
-// articles [articleSchema]
-
-// articleShema {
-    // par1
-    // par2
-    // par3
-    // par4
-    // par5
-    // link1
-    // link2
-    // img1
-    // img2
-// }
-
-// commentSchema
-// {postId
-// string}
-
-// user
-// can comment can remove comment
-// username 
-// email
-// password
-// comments [
-// commentSchema
-// ]
-
+// here we are bundling everything in our models files to be used in the future
+module.exports = { Article, User, Comment, Admin };
