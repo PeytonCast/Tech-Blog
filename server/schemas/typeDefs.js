816573e38e50b3273780c73c069d23523c432d15
Peyton C <peyton.castill0@icloud.com>
2023-02-20 11:53:26 -0500

    some basic typeDefs to validate the resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,38 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-// write code here
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+    }
+
+    type Admin {
+        _id: ID!
+        username: String!
+        password: String!
+    }
+
+    type AdminAuth {
+        token: ID! 
+        admin: Admin
+    }
+    type UserAuth {
+        token: ID! 
+        user: User
+    }
+
+    type Query {
+        getAdmin: Admin
+        getUser: User 
+    }
+
+    type mutation{
+        userLogin(email: String!, password: String!): UserAuth
+        addUser(username: String!, email: String!, password: String!): UserAuth
+        adminLogin(username: String!, password: String!): AdminAuth
+        addAdmin(username: String!, password: String!): adminAuth
+    }
 `
+module.exports = typeDefs
